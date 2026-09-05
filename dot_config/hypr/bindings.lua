357c2150e0e06069efe82891fa074628c0f9343a
Dmitriy Hellwig <mr.lyssov.dmitriy@gmail.com>
2026-09-05 20:02:59 +0200

    download video and zsh

--- dot_config/hypr/bindings.lua
+++ dot_config/hypr/bindings.lua
@@ -6,3 +6,6 @@ hl.unbind("SUPER + L")
 
 o.bind("SUPER + H", "Focus on left window", hl.dsp.focus({ direction = "l" }))
 o.bind("SUPER + L", "Focus on right window", hl.dsp.focus({ direction = "r" }))
+
+-- Download the active browser tab to ~/Videos.
+o.bind("SUPER + ALT + D", "Download video", os.getenv("HOME") .. "/.local/bin/download-video")
